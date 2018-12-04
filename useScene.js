5f1169844ad615efa6a2cca2e62905d96e2d702c
DESKTOP-RLCKICF\Ace <tttriet1997@gmail.com>
2018-12-04 23:46:10 +0700

    ace

---
+++ useScene.js
@@ -0,0 +1,65 @@
+// Questa funzione attiva i movimenti della telecamera in una certa direzione
+function activateMove(key) {
+	switch(key) {
+		case 37:
+			rotateRight=true;
+			break;
+		case 39:
+			rotateLeft=true;
+			break;
+	}
+}
+
+// Questaa funzione deattiva il movimento della telecamera in una data direzione
+function deactivateMove(key) {
+	switch(key) {
+		case 37:
+			rotateRight=false;
+			break;
+		case 39:
+			rotateLeft=false;
+			break;
+	}
+}
+
+// La funzione aggiorna effettivamente la posizione della telecamera
+function update() {
+// Controllo se sta avvenendo una rotazione di fine turno
+	if(changeViewpoint) {
+		if(turn=="b") {
+			if(change!=Math.PI) {
+				change+=Math.PI/8;
+				camera.position.x=Math.sin(change)*10;
+				camera.position.z=Math.cos(change)*10;
+				camera.lookAt(scene.position);
+			} else {
+				rotation=Math.PI;
+				changeViewpoint=false;
+			}
+		} else {
+			if(change!=0) {
+				change-=Math.PI/8;
+				camera.position.x=Math.sin(change)*10;
+				camera.position.z=Math.cos(change)*10;
+				camera.lookAt(scene.position);
+			} else {
+				rotation=0;
+				changeViewpoint=false;
+			}
+		}
+// Altrimenti controllo se sto ruotando a mano
+	} else {
+		if(rotateRight) {
+			rotation+=0.1;
+			camera.position.x=Math.sin(rotation)*10;
+			camera.position.z=Math.cos(rotation)*10;
+			camera.lookAt(scene.position);
+		}
+		if(rotateLeft) {
+			rotation-=0.1;
+			camera.position.x=Math.sin(rotation)*10;
+			camera.position.z=Math.cos(rotation)*10;
+			camera.lookAt(scene.position);
+		}
+	}
+}
